4ad577b02a77ac4b0ef17e331abf3cce8eb87e9d
BINET aurélie <au.binet21@gmail.com>
2024-03-20 08:54:24 +0100

    user1

---
+++ Applications.php
@@ -0,0 +1,44 @@
+
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">ARCADIA </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Accueil</a>
+          <div class="">
+          <a href="Index.php">Index</a>
+        </li>
+        <li class="nav-item">
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+           Services
+                  <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Restauration</a></li>
+            <li><a class="dropdown-item" href="#">Visite des habitats</a></li>
+            <li><a class="dropdown-item" href="#">Visite du zoo en petit train</a></li>
+          </ul>
+
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Habitats
+          </a>
+          <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Connexion
+          </a>
+          <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Contact
+          </a>
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
---
+++ index.php
@@ -0,0 +1,38 @@
+<?php include "header.php"
+?> 
+<h1 class="text-danger">ARCADIA</h1>
+<h3>
+    
+    Arcadia est située en France,près de la forêt de brocéliande ,en bretagne, depuis 1960.
+    Ils possèdent tout un panel d'animaux et essaye de reconstituer à l'identique leurs habitats :
+    savane,
+    marais,
+    jungle,
+    Les animaux sont inspectés chaque jour par un vétérinaire.
+
+    D'un point de vue financier ,le zoo se porte bien ,cela fait plaisir à José,directeur de ce zoo. 
+    A ce jour, José n'a pas de site web pour son zoo ,par conséquent il a mandaté Josette pour 
+    trouver une entreprise ,qui va lui permettre de créer son site web.
+    Après l'obtention du diplome , j'ai été mandaté au sein de la société DEVSoft 
+    pour réaliser leur site web.
+    Précision importante de José:“Je souhaite avoir une
+application web dont les couleurs et le thème font penser à de l’écologie, le zoo est
+entièrement indépendant au niveau des énergies et il faut que lorsqu’un utilisateur consulte
+l’application, il ressente les valeurs de l’écologie dont nous sommes fiere”</h3>  
+
+
+
+<h3> Les différents habitats sont la Savane;la Jungle;le Marais.
+    Les animaux sont :
+        les singes,
+        les éléphants,
+        les oiseaux,
+        les crocodiles,
+        les dauphins,
+        les otaries et bien plus encore</h3>
+    <h3> Zoom sur les avis du zoo: Très beau parc;
+                                Un immense zoo ,à découvrir;
+                                Les animaux sont heureux ,on le resssent.</h3>            
+                                             
+
+ <?php include 'footer.php' ?>
