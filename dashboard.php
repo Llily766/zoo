ee934f3aaec22a9421489eb7d52fe01c6826e21e
BINET aurélie <au.binet21@gmail.com>
2024-03-26 12:29:01 +0100

    modification dashboard et images sur us3

--- dashboard.php
+++ dashboard.php
@@ -3,39 +3,41 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $title = $_POST['title'];
     $description = $_POST['description'];
     $category = $_POST['category'];
-    $images = 'images'; 
-    
+    $images = 'images';
+
     // Informations de connexion à la base de données
     $host = 'localhost'; // Adresse du serveur MySQL
-    $dbname = 'aurelieDB'; // Nom de la base de données
+    $dbname = 'zoo'; // Nom de la base de données
     $user = 'votre_nom_utilisateur'; // Nom d'utilisateur MySQL
     $pass = 'votre_mot_de_passe'; // Mot de passe MySQL
-    
+
     // Connexion à la base de données
     try {
-        $dbh = new PDO("mysql:host=$host;dbname=$aurelieDB", 'aurelie', '210982');
+
+        $dbh = new PDO("mysql:host=" . $host . ";dbname=" . $dbname . ";charset=utf8", 'root', '');
+
 
         // Configuration pour afficher les erreurs PDO
         $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        
+
         // Requête d'insertion
-        $sql= "INSERT INTO `service` (`id`, `title`, `description`, `images`, `category_id`) 
-                VALUES (NULL, :title, :description, :images, :category)";
-        
+        $sql = "INSERT INTO `service` (`id`, `nom`, `description`, `images` ) 
+                VALUES (NULL, :nom, :description, :images)";
+
         // Préparation de la requête
         $stmt = $dbh->prepare($sql);
-        
+
         // Liaison des valeurs
-        $stmt->bindParam(':title', $title);
+        $stmt->bindParam(':nom', $title);
         $stmt->bindParam(':description', $description);
         $stmt->bindParam(':images', $images);
-        $stmt->bindParam(':category', $category);
-        
+
+
         // Exécution de la requête
         $stmt->execute();
-        
+
         echo "Enregistrement ajouté avec succès";
-    } catch(PDOException $e) {
+    } catch (PDOException $e) {
         // En cas d'erreur, afficher l'erreur
         echo "Erreur : " . $e->getMessage();
     }
@@ -61,5 +63,3 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     </div>
     <button type="submit">Enregistrer</button>
 </form>
-
-
---
+++ habitats.php
@@ -0,0 +1,3 @@
+<?php
+echo '<img src="assets/habitas/elephant.jpg" alt="">';
+?>
